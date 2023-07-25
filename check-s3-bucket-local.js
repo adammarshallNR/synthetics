b5a43a3cf8d5781275e649cf27ab0cf2042df1c1
adammarshallNR <111354481+adammarshallNR@users.noreply.github.com>
2023-07-25 12:34:39 +0100

    Initial Commit of tested script for S3

---
+++ check-s3-bucket-local.js
@@ -0,0 +1,74 @@
+var assert = require("assert")
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set thresholds and other variables
+const config = {
+    minFileSize:5000
+    accessKeyIdLocal:'IMPORTFROMFILE/PROFILEETC'
+    secretAccessKeyLocal:'IMPORTFROMFILE/PROFILEETC'
+};
+/* 
+Set the Bucket name & object returned - these are used when calling the main function getObjectDetails.  
+In most cases using this synthetic to check a file that is uploaded daily, the filename will remain the same, or 
+perhaps with a datestamp, or will be the latest one, and the script can be modified as such - if modifying to look
+for the latest file, make sure there is a check to ensure it has also been uploaded within the last X hours or the
+same day the synthetic executes on (in UTC)
+*/
+const bucketDetails = {
+    Bucket: "synthetic-monitor-test-adam-marshall",
+    File: "generated.json"
+   };
+
+// Create S3 service object
+var s3 = new AWS.S3(
+  {
+    apiVersion: '2006-03-01',
+    region: 'eu-west-2',
+    accessKeyId: config.accessKeyIdLocal,
+    secretAccessKey: config.secretAccessKeyLocal
+  }
+);
+
+//Gets all the objects in the specified bucket
+async function getObjectDetails(bucketName, fileName) {
+  let object;
+
+  try {
+    const params = {
+      Bucket: bucketName
+    };
+
+    const response = await s3.listObjects(params).promise();
+
+    if (response.Contents.length > 0) {
+      object = response.Contents.find(item => item.Key === fileName);
+
+      if (!object) {
+        throw new Error(`No object with key "${fileName}" found in the bucket`);
+      }
+    } else {
+      throw new Error('No objects found in the bucket');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+
+  return object; 
+}
+
+  //Do the work and compare the sizes
+  (async () => {
+    try {
+      const object = await getObjectDetails(bucketDetails.Bucket, bucketDetails.File);
+      console.log(object);
+        // Assertion
+         if (object.Size > config.minFileSize) {
+          assert.ok(true); // Object size is greater than config.minfilesize
+        } else {
+         assert.ok(false); // Object size is not greater than config.minfilesize
+  }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  })();
